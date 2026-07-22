e2f594620def4ff9fe77d6b681eb988d98fcbb93
CodeRonin404 <tukusalifyasichali@gmail.com>
2026-07-23 01:36:43 +0200

    Update: added constants for the site names.

--- config/constants.py
+++ config/constants.py
@@ -1,1 +1,9 @@
+# BASE URL's
 GOZAMBIAJOBS_URL = "https://gozambiajobs.com/jobs"
+JOBWEBZAMBIA_URL = "https://jobwebzambia.com/job-category"
+
+# SOURCE NAMES
+GOZAMBIAJOBS_NAME = "gozambiajobs"
+JOBWEBZAMBIA_NAME = "jobwebzambia"
+
+
