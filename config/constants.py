f990325a3ba6386c8946c2fb61aa6df0f04bdc1f
CodeRonin404 <tukusalifyasichali@gmail.com>
2026-07-23 22:42:09 +0200

    Update: changed the constants for the scrapers.

--- config/constants.py
+++ config/constants.py
@@ -3,7 +3,7 @@ GOZAMBIAJOBS_URL = "https://gozambiajobs.com/jobs"
 JOBWEBZAMBIA_URL = "https://jobwebzambia.com/job-category"
 
 # SOURCE NAMES
-GOZAMBIAJOBS_NAME = "gozambiajobs"
-JOBWEBZAMBIA_NAME = "jobwebzambia"
+GOZAMBIAJOBS_NAME = "GoZambiaJobs"
+JOBWEBZAMBIA_NAME = "JobWebZambia"
 
 
