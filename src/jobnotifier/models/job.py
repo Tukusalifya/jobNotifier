aac7de60e94dec5ad415cdaf5ea17b3365ee97bc
CodeRonin404 <tukusalifyasichali@gmail.com>
2026-07-23 22:38:23 +0200

    Update: code cleanup.

--- src/jobnotifier/models/job.py
+++ src/jobnotifier/models/job.py
@@ -12,10 +12,10 @@ class Job:
     category: str
     url: str
     type: str
+    source: str
 
     normalized_category: str | None = None
     posted_date: str | None = None
-    source: str = "gozambiajobs"
 
     scraped_at: str = field(
         default_factory=lambda: datetime_formatter(datetime_object=datetime.now(),
