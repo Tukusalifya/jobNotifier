d12084165552bcc507d8319d8d6bd0f2385c5d1f
CodeRonin404 <tukusalifyasichali@gmail.com>
2026-07-29 00:22:47 +0200

    Update: changed variable names for the settings class.

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,8 @@ BASE_DIR = Path(__file__).resolve().parents[1]
 
 
 class Settings:
-    SMTP_HOST = os.getenv("SMTP_HOST", "localhost")
-    SMTP_PORT = int(os.getenv("SMTP_PORT", "1025"))
+    SMTP_SERVER = os.getenv("SMTP_SERVER", "smtp.gmail.com")
+    SMTP_PORT = int(os.getenv("SMTP_PORT", "587"))
     SMTP_USERNAME = os.getenv("SMTP_USERNAME", "")
     SMTP_PASSWORD = os.getenv("SMTP_PASSWORD", "")
     EMAIL_SENDER = os.getenv("EMAIL_SENDER", "test@example.com")
